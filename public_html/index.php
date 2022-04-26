b7cec885696086394ddada06e3188054c62524d4
cecile6482 <cecile.elshtby@gmail.com>
2022-04-26 09:10:22 +0200

    màj include

--- public_html/index.php
+++ public_html/index.php
@@ -1,44 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-
-    <meta charset="UTF-8" />
-    <meta name="description" content="Activité PHP promo SIMPLON Boulogne sur Mer" />
-    <meta name="keywords" content="HTML, CSS, PHP, Bootstrap, Bootswatch, SIMPLON, Boulogne" />
-    <meta name="author" content="Nicolas HERBEZ" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-
-    <title>PHP - Procédural</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/spacelab/bootstrap.min.css">    
-
+    <?php include("./includes/head.inc.html"); ?> 
 </head>
 
 <body>
-<header class="d-none d-sm-block">
 
-<div class="card bg-dark rounded-0">
-    <div class="row g-0">
-
-        <div class="col-md-5">
-            <img src="../images/php.png" alt="logo php" class="img-fluid rounded-start m-3">
-        </div>
-
-        <div class="col-md-7">
-            <div class="card-body text-white">
-                <h1 class="card-title">PHP procédural</h1>
-                <p class="card-text">Créer le back-end permettant de sauvegarder des données en session à travers un formulaire 🙂  </p>
-                <p class="card-text"><small class="text-muted">J'apprends et je me perfectionne</small></p>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-<div class="p-2 bg-dark mt-2"></div>
-<div class="p-1 bg-dark mt-2 mb-3"></div>
-
+<header>
+    <?php include("./includes/header.inc.html"); ?> 
 </header>
 
     
