db8fb38af303db1f71e7ea36576a8abaab5c5b42
cecile6482 <cecile.elshtby@gmail.com>
2022-04-27 15:16:37 +0200

    version 1.2.2

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
+
     <?php include("./includes/head.inc.html"); ?> 
-</head>
 
 <body>
 
-<header>
     <?php include("./includes/header.inc.html"); ?> 
-</header>
+
 
 
     
