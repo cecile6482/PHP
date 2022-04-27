ba6332149a8d5b91c2272d995368947ca873b6f6
cecile6482 <cecile.elshtby@gmail.com>
2022-04-27 19:33:22 +0200

    version 1.2.3

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -7,8 +13,33 @@
 
     <?php include("./includes/header.inc.html"); ?> 
 
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <nav>
+                    <div class="d-grid gap-2">
+                    
+                            <button type="button" class="btn btn-outline-secondary btn-block">Home</button>
+                            <?php 
+                                if(!empty($_SESSION)){
+                                    include("./includes/ul.inc.html");
+                                    $table = $_SESSION["table"];
+                                }
+                            ?>
+                    </div>
+                </nav>
+            </div>
 
+            <div class="col-9">         
+                <section>
+                    <button type="button" class="btn btn-primary">Ajouter des données</button>
+                    
+                    <?php  include 'includes/form.inc.html'; ?>
+                </section>
+            </div>
+        </div>   
+    </div>
 
-    
+    <?php include("./includes/footer.inc.html"); ?> 
 </body>
 </html>
