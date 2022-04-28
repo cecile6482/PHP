4225e86f2e17ba2198f3d7fdbb959f6bfd156380
cecile6482 <cecile.elshtby@gmail.com>
2022-04-28 23:54:48 +0200

    version 1.2.6 (night shift)

--- public_html/index.php
+++ public_html/index.php
@@ -48,13 +48,53 @@ if(isset($_SESSION['table'])) $table = $_SESSION['table'];
                         
                         
                     } elseif(isset($_GET["debugging"])) {
-                        echo '<h2>Débogage</h2>';
+                        echo '<h2 class="text-center">Débogage</h2>';
                         echo "<p>===> Lecture du tableau à l'aide de la fonction print_r()</p>";
                         print "<pre>";
                         print_r($table);
                         print "</pre>";
                     
-                    } else if (isset($_GET['del'])) {
+                    } elseif (isset($_GET['concatenation'])) {
+
+                        echo '<h2 class="text-center">Concaténation</h2><br>';
+        
+                        echo "<h3 class='fs-5'>===> Construction d'une phrase avec le contenu du tableau :</h3>";
+                        if ($sex == "femme") { echo ("Mme"); } else { echo ("Mr");}
+                        echo "<p>" . $table["first_name"] . " " . $table["last_name"] . " <br>J'ai " . $table["age"] . " ans et je mesure " . $table["size"] . "m.</p><br>";
+
+                        echo "<h3 class='fs-5'>===> Construction d'une phrase après MAJ du tableau :</h3><br><br>";
+
+
+                        echo "<h3 class='fs-5'>===> Affichage d'une virgule à la place du point pour la taille :</h3>";
+                        echo "<p>" . $table["first_name"] . " " . $table["last_name"] . " <br>J'ai " . $table["age"] . " ans et je mesure " . str_replace('.', ',', $table['size']) . "m.</p>";
+        
+                    
+                    } else if (isset($_GET['loop'])) {
+
+                        echo "<h2 class='text-center'>Boucle</h2><br>";
+                        echo "<p>===> Lecture du tableau à l'aide d'une boucle foreach</p><br>";
+                        $table = $_SESSION['table'];
+                        $i = 0;
+                        foreach ($table as $item => $value) {
+                            echo '<div>à la ligne n°' . $i . ' correspond à la clé "' . $item . '" et contient "' . $value . '"</div>';
+                            $i++;
+                        }
+                    
+                    } else if (isset($_GET['function'])){     
+
+                        echo "<h2 class='text-center'>Fonction</h2><br>";
+                        echo "<p>===> J'utilise ma fonction readTable()</p><br>";
+                        function readTable(){
+                            $table = $_SESSION['table'];
+                            $i = 0;
+                            foreach ($table as $item => $value) {
+                                echo '<div>à la ligne n°' . $i . ' correspond à la clé "' . $item . '" et contient "' . $value . '"</div>';
+                                $i++;
+                            }
+                        }  
+                        readTable();   
+                    
+                    } elseif (isset($_GET['del'])) {
                         unset ($_SESSION['table']);
                         if (empty($_SESSION['table'])) {
                             echo '<p class="alert-success text-center py-3"> Données suprimées</p>';
